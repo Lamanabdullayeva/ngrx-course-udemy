df92b444bc074ec566127c38438113f969a3cec3
Abdullayeva <la.abdullayeva99@gmail.com>
2025-02-26 15:06:57 +0100

    action and reducer for login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,42 +1,53 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
-import {Store} from "@ngrx/store";
+import { Store } from "@ngrx/store";
 
-import {AuthService} from "../auth.service";
-import {tap} from "rxjs/operators";
-import {noop} from "rxjs";
-import {Router} from "@angular/router";
+import { Router } from "@angular/router";
+import { noop } from "rxjs";
+import { tap } from "rxjs/operators";
+import { AuthService } from "../auth.service";
+import { login } from "../auth.actions";
+import { AuthActions } from "../action-types";
+import { AuthState } from "../reducers";
 
 @Component({
-    selector: 'login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss'],
-    standalone: false
+  selector: "login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
+  standalone: false,
 })
 export class LoginComponent implements OnInit {
-
   form: FormGroup;
 
   constructor(
-      private fb:FormBuilder,
-      private auth: AuthService,
-      private router:Router) {
-
-      this.form = fb.group({
-          email: ['test@angular-university.io', [Validators.required]],
-          password: ['test', [Validators.required]]
-      });
-
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router,
+    private store: Store<AuthState>
+  ) {
+    this.form = fb.group({
+      email: ["test@angular-university.io", [Validators.required]],
+      password: ["test", [Validators.required]],
+    });
   }
 
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 
   login() {
+    const val = this.form.value;
 
+    this.auth
+      .login(val.email, val.password)
+      .pipe(
+        tap((user) => {
+          console.log("User:", user);
+
+          this.store.dispatch(login({ user }));
+
+          this.router.navigateByUrl("/courses");
+        })
+      )
+      .subscribe(noop, () => alert("Login Failed"));
   }
-
 }
-
