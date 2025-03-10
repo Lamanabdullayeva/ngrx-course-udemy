a28cb8f8ae2f66685a3120d43584a7fe7fb51176
Abdullayeva <la.abdullayeva99@gmail.com>
2025-03-10 13:11:11 +0100

    fetching data done, sorting

--- src/app/courses/reducers/course.reducers.ts
+++ src/app/courses/reducers/course.reducers.ts
@@ -1,20 +1,24 @@
 import { createEntityAdapter, EntityState } from "@ngrx/entity";
 import { createReducer, on } from "@ngrx/store";
 import { CourseActions } from "../action-types";
-import { Course } from "../model/course";
+import { compareCourses, Course } from "../model/course";
 
-export interface CoursesState extends EntityState<Course> {}
+export interface CoursesState extends EntityState<Course> {
+  allCoursesLoaded: boolean;
+}
 
-let state: CoursesState;
+export const adapter = createEntityAdapter<Course>({
+  sortComparer: compareCourses,
+});
 
-export const adapter = createEntityAdapter<Course>();
-
-const initialCoursesState = adapter.getInitialState();
+const initialCoursesState = adapter.getInitialState({
+  allCoursesLoaded: false,
+});
 
 export const coursesReducer = createReducer(
   initialCoursesState,
   on(CourseActions.allCoursesLoaded, (state, action) => {
-    return adapter.setAll(action.courses, state);
+    return adapter.setAll(action.courses, { ...state, allCoursesLoaded: true });
   })
 );
 
