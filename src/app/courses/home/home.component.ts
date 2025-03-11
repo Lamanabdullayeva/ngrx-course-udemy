510ac3b85f0a30551edae4a9a20876d0f411e4b1
Abdullayeva <la.abdullayeva99@gmail.com>
2025-03-11 15:21:58 +0100

    lesson entity

--- src/app/courses/home/home.component.ts
+++ src/app/courses/home/home.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from "@angular/core";
-import { compareCourses, Course } from "../model/course";
-import { Observable } from "rxjs";
-import { defaultDialogConfig } from "../shared/default-dialog-config";
-import { EditCourseDialogComponent } from "../edit-course-dialog/edit-course-dialog.component";
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
-import { map, shareReplay } from "rxjs/operators";
-import { CoursesHttpService } from "../services/courses-http.service";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { EditCourseDialogComponent } from "../edit-course-dialog/edit-course-dialog.component";
+import { Course } from "../model/course";
 import { CourseEntityService } from "../services/course-entity.service";
+import { defaultDialogConfig } from "../shared/default-dialog-config";
 
 @Component({
   selector: "home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnInit {
   promoTotal$: Observable<number>;
